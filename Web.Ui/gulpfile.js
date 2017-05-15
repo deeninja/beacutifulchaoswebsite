1a1e51b25acba07600d6527a1b0876ef2d9d0887
William <william@techsys.co.za>
2017-05-15 15:30:11 +0200

    + initial setup, packages installed, index view rendering

---
+++ Web.Ui/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
+
+var minify = require('gulp-minify');
+
+gulp.task('compress', function() {
+    gulp.src('wwwroot/lib/*.js') // minify all js files in lib
+        .pipe(minify({
+            ext: { // output source
+                src: '-debug.js',
+                min: '.js' // file extension of minfied files
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.combo.js', '-min.js'] // ignore these file types
+
+        }))
+        .pipe(gulp.dest('wwwroot/dist')); // opens managed data stream to output result to dist
+})
